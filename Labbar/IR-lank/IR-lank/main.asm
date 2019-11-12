07ed708156ae8c96936118855966bb4271196fec
Joey Strandnes <joey.makdesi1@hotmail.com>
2019-11-12 13:53:06 +0100

    Final

--- Labbar/IR-lank/IR-lank/main.asm
+++ Labbar/IR-lank/IR-lank/main.asm
@@ -1,62 +1,70 @@
 ;
-; IR-lank.asm
-;
-; Created: 2019-11-08 12:25:30
-; Author : josst471
-;
+; AssemblerApplication1.asm
+; LAB1 IR
+; Created: 2019-11-05 18:00:30
+; Author : Johan
 
+SETUP:	
+	ldi		r22,$FF					; Utport till displayen+scop
+	out		DDRB,r22				; Utport till displayen+scop
+	
+	ldi		r18, HIGH(RAMEND)
+	out		SPH, r18
+	ldi		r18, LOW(RAMEND)
+	out		SPL, r18
 
-	ldi r21, $00
-	ldi r23, $00 ; Startbit counter
-	ldi r20, $00
-
-	ldi r22, $0F
-	out DDRB, r22
-
-	;in r21, PINA
+	clr		r21						; DATA
+	clr		r19						; Sort
+	ldi		r20, 4					; COUNTER 1
+	ldi		r18, 4					; COUNTER 2
 
 START_BIT:
+	sbis	PINA,0 
+	rjmp	START_BIT
+	ldi		r16, 31
+	call	DELAY
 
-	sbis PINA, 0
-	brne START_BIT
-	ldi r16, 31
-	clr r23
 
-DELAY:
-	sbi PORTB, 7
-delayYttreLoop:
-	ldi r17, $1F
-delayInreLoop:
-	dec r17
-	brne delayInreLoop
-	dec r16
-	brne delayYttreLoop
-	cbi PORTB, 7
-
-	inc r23
-
+DOUBLECHECK:
+	sbis	PINA,0
+	rjmp	START_BIT
+	call	DELAY
+		
 
 DATA:
+	lsl		r21 
+	sbic	PINA,0
+	inc		r21					;ökar r21 enbart om Pin0 är hög
+	dec		r20					;Counter++
+	breq	PRINT
+	;brne	PRINT
+	call	DELAY
+	;sbrc	r20, 0
+	rjmp	DATA
+
+PRINT:
 	
-	ldi r16, 62
 	
-	cpi r23, 2
-	brne DELAY
-
-	in r20, PINA
-	lsl r20
-
-	cpi r23, 3
-	brne DELAY
-
-	in r20, PINA
-	lsl r20
-
-	;sbic PINA, 0
-	;ldi r22, 1
-	;ldi r22, 0
-	
+	lsl		r19
+	lsr		r21
+	brcc	NOCARRY
+	inc		r19
+NOCARRY:	
+	dec		r18;counter++
+	brne	PRINT
+	out		PORTB,r19			; fortfarande LSB/MSB bakvända
+	rjmp	SETUP
 
 
-	inc r21
-	jmp DELAY
+DELAY:
+	sbi		PORTB,7
+delayYttreLoop:
+	ldi		r17,$1F
+delayInreLoop:
+	dec		r17
+	brne	delayInreLoop
+	dec		r16
+	brne	delayYttreLoop
+	cbi		PORTB,7	
+	ldi		r16,62
+	ret	
