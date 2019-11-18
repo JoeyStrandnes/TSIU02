56fe3f2271c57bddff160276f8245810b7bdabcb
txcpnae <57444167+txcpnae@users.noreply.github.com>
2019-11-18 18:14:56 +0100

    Update main.asm

--- Labbar/Morsesandare/Morsesandare/main.asm
+++ Labbar/Morsesandare/Morsesandare/main.asm
@@ -1,5 +1,5 @@
-;En kort signal (îprickî/îditî) ‰r 1 tidsenhet lÂng
-;En lÂng signal (îstreckî/îdahî) ‰r 3 tidsenheter
+;En kort signal (‚Äùprick‚Äù/‚Äùdit‚Äù) √§r 1 tidsenhet l√•ng
+;En l√•ng signal (‚Äùstreck‚Äù/‚Äùdah‚Äù) √§r 3 tidsenheter
 ;Mellan teckendelarna skall det vara 1 tidsenhets tystnad
 ;Mellan tecknen 3 tidsenheters tystnad
 ;Mellan ord 7 tidsenheters tystnad.
@@ -9,27 +9,31 @@ SETUP:
 	out	SPH, r16
 	ldi	r16, LOW(RAMEND)
 	out	SPL, r16
-	;ldi	r16, $0F
-	;out	DDRB, r16
-
+	ldi	r16, $FF
+	out	DDRB, r16
 	ldi	ZL, LOW(MESSAGE*2)
 	ldi	ZH, HIGH(MESSAGE*2)
-
+	.def N=r18
 MORSE:
 	call	GET_CHAR
+	cpi		r21,$00
+	breq	END_OF_LINE ; loopar tills vi st√∂ter p√• $00-d√• till END_OF_LINE
 	
-	jmp	MORSE
+	call	LOOKUP
+	call	SEND
+	ldi		N,$02
+	call	NO_BEEP ; N√§sta bokstav kr√§ver 3N tystnad innan den s√§nds. Har gjort en innan i SEND 
+	jmp		MORSE 
+
+END_OF_LINE:
+	ldi		N,$07
+	call	NO_BEEP
+	jmp		SETUP
 ;---------------------------------------------------------------------
 GET_CHAR:
 	lpm	r21, Z+
-	breq	END_OF_LINE
-	;call	NOBEEP ; N‰sta bokstav kr‰ver 3N tystnad innan den s‰nds
-	call	LOOKUP
-	jmp	GET_CHAR ; loopar tills vi stˆter pÂ $00-dÂ till END_OF_LINE
-
-END_OF_LINE:		
 	ret
-
+	;pop Z+??
 ;---------------------------------------------------------------------
 LOOKUP:
 	push	r21
@@ -37,7 +41,7 @@ LOOKUP:
 	push	ZH
 	ldi	ZH, HIGH(BTAB*2)
 	ldi	ZL, LOW(BTAB*2)
-	subi	r21, $41 ;gˆr om till en offset i alfabetet
+	subi	r21, $41 ;g√∂r om till en offset i alfabetet
 	add	ZL, r21
 	brcc	NO_CARRY_TO_ZH
 	inc	ZH
@@ -49,24 +53,63 @@ NO_CARRY_TO_ZH:
 	pop	r21
 	ret
 ;----------------------------------------------------------------------	
+SEND:
+	call	GET_BIT
+	brcc	DIT
+DAT:
+	ldi		N,$02
+	call	BEEP
+DIT:
+	ldi		N,$01
+	call	BEEP
+TEST:
+	ldi		N,$01
+	call	NO_BEEP;PAUS mellan varje dit/dat
 
+	cpi		r22,$80
+	brne	SEND
+	ret
+;----------------------------------------------------------------------
 GET_BIT:
-	push	r22
-
 	lsl	r22
+	ret
+;---------------------------------------------------------------------
+BEEP:
+	ldi		r16,$FF
+	out		DDRB,r16
+	call	DELAY
+	dec		N
+	breq	BEEP_DONE
+	rjmp	BEEP
+BEEP_DONE:
+	ret
+;--------------------------------------------------------------------
 
-
+NO_BEEP:
+	ldi		r16,$00
+	out		DDRB,r16
+	call	DELAY
+	dec		N
+	breq	NO_BEEP_DONE
+	rjmp	NO_BEEP
+NO_BEEP_DONE:
+	ret
+;---------------------------------------------------------------------
 DELAY:
-	ldi		r16,200
+	ldi		r16,200 ;200 1 tidsenhet l√•ng
 delayYttreLoop:
-	ldi		r17,$1F
+	ldi		r17,$FF;1F
 delayInreLoop:
+	ldi		r19,$FF
+	out		PORTB,r19
 	dec		r17
+	ldi		r19,$00
+	out		PORTB,r19
 	brne	delayInreLoop
 	dec		r16
 	brne	delayYttreLoop
 	ret
-
+;---------------------------------------------------------------------
 
 
 .org $0100
@@ -76,3 +119,5 @@ MESSAGE:
 .org $0150
 BTAB:
 	.db $60, $88, $A8, $90, $40, $28, $D0, $08, $20, $78, $B0, $48, $E0, $A0, $F0, $68, $D8, $50, $10, $C0, $30, $18, $70, $98, $B8, $C8
+
+
