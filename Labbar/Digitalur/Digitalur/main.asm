8eddf3319b369cd0ad1837f40b40ba1c81b5b1d2
Joey Strandnes <joey.makdesi1@hotmail.com>
2019-11-27 14:59:54 +0100

    16-bit Timer

--- Labbar/Digitalur/Digitalur/main.asm
+++ Labbar/Digitalur/Digitalur/main.asm
@@ -6,23 +6,32 @@ Timer for real time counting/ keeping time
 
 */
 
+.org 0x00
+jmp SETUP
+
+.org 0x10 
+jmp ISR_TIMER0
+
+.org 0x30
+
 SETUP: 
 	
 	.def TIME = r22
-	ldi TIME, 60
+	ldi TIME, 14
 
+	ldi	r16, HIGH(RAMEND)
+	out	SPH, r16
+	ldi	r16, LOW(RAMEND)
+	out	SPL, r16
 
+TIMER0_INIT: ;16-bit timer 0 set as an overflow timer 
 
-TIMER0_INIT: ;8-bit timer 0 set as an overflow timer 
-
-	ldi r16,(1<<CS12)|(1<<CS10) ;Prescaler set to F_CPU/1024 (S:108)
-	out TCCR1A, r16
-	ldi r16,1<<TOV0
-	out TCCR1B,r16 ; Clear interrupts
-	ldi r16,1<<TOIE0
-	sts TOIE1,r16 ; Interrup enable
+	ldi r16,(1<<CS11)|(1<<CS10) ;Prescaler set to F_CPU/64 (S:108) ~4.1S
+	out TCCR1B, r16
+	ldi r16, 1<<TOIE1
+	out TIMSK, r16
+	sei
 	;ret
-
 
 MAIN:
 	call loop
@@ -38,7 +47,8 @@ ISR_TIMER0:
 	pop r16
 	out SREG,r16
 	pop r16
-	reti
+	jmp MAIN
+	
 TIMER_COUNTER:
 	dec TIME
 	breq MAIN
@@ -54,5 +64,5 @@ L1: dec  r19
     dec  r18
     brne L1
     nop
-	rjmp MAIN
+	ret; MAIN
 
